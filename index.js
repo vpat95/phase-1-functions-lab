ccc8061bc8f4af21b4a1df020dfff48a9675c3bb
Vivek Patel <vpatel360@gmail.com>
2023-06-13 13:09:52 -0400

    commit tree 290bcf0f895587b72862c0c7adf043feba21929e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    return Math.abs(42-distance)
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start-destination)*264)
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start,destination) > 400 && distanceTravelledInFeet(start,destination) <= 2000){
+        return (distanceTravelledInFeet(start,destination) -400)* 0.02 
+    }
+    else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) <= 2500){
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    } 
+}
